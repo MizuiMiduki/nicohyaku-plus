e22e96bab93b9f12ef63864267f294c531e23ac2
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-06-05 17:20:06 +0900

    R3.0

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 $(function () {
-    $('.a-area_sns').after('<button id="nicohyaku_plus__on_off_button" class="nicohyaku-plus--on-off-button-disable">シロナガスクジラ対策</buton>');
+    $('.st-space_right').eq(2).after('<button id="nicohyaku_plus__on_off_button" class="nicohyaku-plus--on-off-button-disable">シロナガスクジラ</buton>');
 });
 
-$(function() {
-$('#nicohyaku_plus__on_off_button').click(function(){
-    $(".auto").toggleClass("nicohyaku-plus--auto-hdn");
-    $(".auto-hdn").toggleClass("nicohyaku-plus--auto");
-    $("#nicohyaku_plus__on_off_button").toggleClass("nicohyaku-plus--on-off-button-disable");
-    $("#nicohyaku_plus__on_off_button").toggleClass("nicohyaku-plus--on-off-button-enable");
-})
+$(function () {
+    chrome.storage.local.get(["nikohyaku_plus_on_off"], function (result) {
+        var on_off = result.nikohyaku_plus_on_off;
+        if (on_off == true) {
+            $(".auto").toggleClass("nicohyaku-plus--auto-hdn");
+            $(".auto-hdn").toggleClass("nicohyaku-plus--auto");
+            $("#nicohyaku_plus__on_off_button").toggleClass("nicohyaku-plus--on-off-button-disable");
+            $("#nicohyaku_plus__on_off_button").toggleClass("nicohyaku-plus--on-off-button-enable");
+        }
+        on_off_toggle = on_off;
+    })
+
+    $('#nicohyaku_plus__on_off_button').click(function () {
+        $(".auto").toggleClass("nicohyaku-plus--auto-hdn");
+        $(".auto-hdn").toggleClass("nicohyaku-plus--auto");
+        on_off_toggle = !on_off_toggle;
+        chrome.storage.local.set(
+            {
+                "nikohyaku_plus_on_off": on_off_toggle,
+            }
+        );
+        $("#nicohyaku_plus__on_off_button").toggleClass("nicohyaku-plus--on-off-button-disable");
+        $("#nicohyaku_plus__on_off_button").toggleClass("nicohyaku-plus--on-off-button-enable");
+    })
 });
